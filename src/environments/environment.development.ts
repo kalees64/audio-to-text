ce0e192f27b4a070979108273f92c3b04355a4c8
Kaleeswaran V <vkalees64@gmail.com>
2024-11-26 10:07:05 +0530

    fix: env fix

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  ASSEMBLYAI_KEY: process.env['ASSEMBLYAI_KEY'],
+  ASSEMBLYAI_UPLOAD_API: process.env['ASSEMBLYAI_UPLOAD_API'],
+  OPENAI_API: process.env['OPENAI_API'],
+  OPENAI_KEY: process.env['OPENAI_KEY'],
+};
